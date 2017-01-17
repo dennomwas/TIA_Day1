2a18e9418c64f0d6b085ad51de87ba4fa1123416
Dennis Mwangi <githinji.mwangi@gmail.com>
2017-01-17 18:33:38 +0300

    fizzbuzz lab

--- data_types.py
+++ data_types.py
@@ -1,3 +1,4 @@
+
 def data_type(data):
 	if type(data) == None:
 		return "No value"
@@ -20,7 +21,15 @@ def data_type(data):
 
 
 
+# endNumber = int(raw_input("Enter the end number here "))
 
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-2) + fib(n-1)
+
+# print map(fib, range(0, endNumber)
+print (7)
 
 
 	
