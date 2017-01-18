06ef062b05bb1bee3fd263cedf2561511269e560
dennomwas <dennomwas@users.noreply.github.com>
2017-01-18 17:51:52 +0300

    function to return varied output based on input
    I have instantiated the class

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,14 +3,14 @@
 #a number that is divisible by, 3, 5, or both 3 and 5, respectively.
 #When the number is not divisible by 3 or 5,the number itself should be returned.
 def fizz_buzz(num):
-	if (num % 3 == 0):
-		return "Fizz"
-	elif (num == 3):
-		return "Fizz Buzz"
-	elif (num % 5 == 0 ):
+	if (num % 15 == 0):
+		return "FizzBuzz"
+	elif (num % 5 == 0):
 		return "Buzz"
-	elif (num == 5):
-		return "Fizz Buzz"
+	elif (num % 3 == 0 ):
+		return "Fizz"
 	else:
 		return num 
+
+print(fizz_buzz(45))
 
