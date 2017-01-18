ca24cc4563243ebea3e54a6396e0f5faba329669
dennomwas <dennomwas@users.noreply.github.com>
2017-01-18 17:42:42 +0300

    a program checking data types

--- data_types.py
+++ data_types.py
@@ -20,16 +20,4 @@ def data_type(data):
 			return data[2]
 
 
-
-# endNumber = int(raw_input("Enter the end number here "))
-
-def fib(n):
-    if n < 2:
-        return n
-    return fib(n-2) + fib(n-1)
-
-# print map(fib, range(0, endNumber)
-print (7)
-
-
 	
